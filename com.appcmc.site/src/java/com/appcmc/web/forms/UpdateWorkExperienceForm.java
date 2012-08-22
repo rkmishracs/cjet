b116998a96577f610dcc5c6ff154503d76fd5b48
hasan143anwar <hasan143anwar@gmail.com>
2012-08-22 12:45:25 +0000

    Added a new UpdateWorkExperienceForm.java file by Hasan on 22-08-2012

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/UpdateWorkExperienceForm.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.forms;
+
+/**
+ *
+ * @author CMC
+ */
+public class UpdateWorkExperienceForm {
+    
+    private String title;
+    private String totalExperienceMonths;
+    private String totalExperienceYears;
+    private String keySkills;
+    private String currentEmployer;
+    private String role;
+    private String previousEmployers;
+    private String enrollmentNumber;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTotalExperienceMonths() {
+        return totalExperienceMonths;
+    }
+
+    public void setTotalExperienceMonths(String totalExperienceMonths) {
+        this.totalExperienceMonths = totalExperienceMonths;
+    }
+
+    public String getTotalExperienceYears() {
+        return totalExperienceYears;
+    }
+
+    public void setTotalExperienceYears(String totalExperienceYears) {
+        this.totalExperienceYears = totalExperienceYears;
+    }
+
+    public String getKeySkills() {
+        return keySkills;
+    }
+
+    public void setKeySkills(String keySkills) {
+        this.keySkills = keySkills;
+    }
+
+    public String getCurrentEmployer() {
+        return currentEmployer;
+    }
+
+    public void setCurrentEmployer(String currentEmployer) {
+        this.currentEmployer = currentEmployer;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPreviousEmployers() {
+        return previousEmployers;
+    }
+
+    public void setPreviousEmployers(String previousEmployers) {
+        this.previousEmployers = previousEmployers;
+    }
+
+    public String getEnrollmentNumber() {
+        return enrollmentNumber;
+    }
+
+    public void setEnrollmentNumber(String enrollmentNumber) {
+        this.enrollmentNumber = enrollmentNumber;
+    }
+    
+}
