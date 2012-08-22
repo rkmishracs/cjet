e5118be926ab7fe877855cf2485e20845419d3c0
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-08-22 11:29:56 +0000

    added methods monthlyReport, weeklyReport, dailyReport in ReportsController.java by bhaskar on 22-08-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/web/controller/ReportsController.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/web/controller/ReportsController.java
@@ -33,6 +33,7 @@ import org.springframework.web.context.request.WebRequest;
 public class ReportsController {
     private Contacts contacts = null;
     private StudentService studentService = null;
+    private Student student = null;
     static Logger LOG = Logger.getLogger(ReportsController.class);
     @RequestMapping(method= RequestMethod.GET )
     public String reports(){
@@ -42,7 +43,6 @@ public class ReportsController {
     
     @RequestMapping(method= RequestMethod.GET,value="/all")
     public String allReports(WebRequest request){
-        Student student = null;
         LOG.debug("=================In AllReports Controller");
         AppCmcSpringContext.init();
         studentService = (StudentService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_SERVICE);
@@ -53,14 +53,13 @@ public class ReportsController {
             
            LOG.debug(student);
         }
-        request.setAttribute("student", studentList, WebRequest.SCOPE_SESSION);
+        request.setAttribute("student", studentList, WebRequest.SCOPE_REQUEST);
           return "/master/allEnrolls";
     }
    
     @ResponseBody
     @RequestMapping(method= RequestMethod.GET,value="/studentEnrl",params={"enrlNo"})
     public Student studentReport(@RequestParam String enrlNo) throws JSONException{
-         Student student = null;
          LOG.debug("=================In studentReports Controller");
          //LOG.debug(enrlNo);
          studentService = (StudentService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_SERVICE);
@@ -68,11 +67,51 @@ public class ReportsController {
          LOG.debug(student);
         //contacts = student.getContacts();
         
-     
-         
-     
-       
-          
         return student;
+    }
+    
+    @RequestMapping(method= RequestMethod.GET, value="/monthlyReport")
+    public String monthlyReport(WebRequest request){
+        LOG.debug("=================In monthlyReports Controller");
+        AppCmcSpringContext.init();
+        studentService = (StudentService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_SERVICE);
+        List<Student> studentMonthlyList = studentService.getByCurrentMonth();
+        Iterator<Student> iterator = studentMonthlyList.iterator();
+        while(iterator.hasNext()){
+            student = iterator.next();
+            LOG.debug(student);
+        }
+        request.setAttribute("studentsMonthlyList", studentMonthlyList, WebRequest.SCOPE_REQUEST);
+        return "/master/monthlyReport";
+    }
+    
+    @RequestMapping(method= RequestMethod.GET, value="/weeklyReport")
+    public String weeklyReport(WebRequest request){
+        LOG.debug("=================In weeklyReports Controller");
+        AppCmcSpringContext.init();
+        studentService = (StudentService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_SERVICE);
+        List<Student> studentWeeklyList = studentService.getByCurrentWeek();
+        Iterator<Student> iterator = studentWeeklyList.iterator();
+        while(iterator.hasNext()){
+            student = iterator.next();
+            LOG.debug(student);
+        }
+        request.setAttribute("studentsWeeklyList", studentWeeklyList, WebRequest.SCOPE_REQUEST);
+        return "/master/weeklyReport";
+    }
+    
+    @RequestMapping(method= RequestMethod.GET, value="/dailyReport")
+    public String dailyReport(WebRequest request){
+        LOG.debug("=================In dailyReports Controller");
+        AppCmcSpringContext.init();
+        studentService = (StudentService) AppContext.APPCONTEXT.getBean(ContextIdNames.STUDENT_SERVICE);
+        List<Student> studentDailyList = studentService.getByCurrentWeek();
+        Iterator<Student> iterator = studentDailyList.iterator();
+        while(iterator.hasNext()){
+            student = iterator.next();
+            LOG.debug(student);
+        }
+        request.setAttribute("studentsDailyList", studentDailyList, WebRequest.SCOPE_REQUEST);
+        return "/master/dailyReport";
     }
 }
