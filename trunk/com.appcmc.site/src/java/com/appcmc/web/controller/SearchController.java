6d82bf6d01447c93b6c775659da30b67d2d0e147
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-08-22 11:30:57 +0000

    added SearchController.java to web/controller folder by bhaskar on 22-08-2012

---
+++ trunk/com.appcmc.site/src/java/com/appcmc/web/controller/SearchController.java
@@ -0,0 +1,17 @@
+
+package com.appcmc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+    @RequestMapping(method= RequestMethod.GET)
+    public String showSearchPage(){
+        
+        return "/master/searchData";
+    }
+}
