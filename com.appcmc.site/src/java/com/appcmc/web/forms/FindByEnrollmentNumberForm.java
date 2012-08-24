074b023376d5975b4ae850f7d99bd0aca95d9f3e
kirankumarnayana9 <kirankumarnayana9@gmail.com>
2012-08-24 07:14:31 +0000

    FindByEnrollmentNumberForm.java is added by kiran on 24-08-2012

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/FindByEnrollmentNumberForm.java
@@ -0,0 +1,14 @@
+package com.appcmc.web.forms;
+
+public class FindByEnrollmentNumberForm {
+
+    private String enrollmentId;
+
+    public String getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public void setEnrollmentId(String enrollmentId) {
+        this.enrollmentId = enrollmentId;
+    }
+}
