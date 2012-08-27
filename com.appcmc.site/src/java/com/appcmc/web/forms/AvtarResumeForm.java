465c3c8143313049ca1cc9f94e8be04d2f9fd512
zarvis.a <zarvis.a@gmail.com>
2012-08-27 05:26:25 +0000

    Added AvtarResumeForm.java for the App by Sudarsan on 27-08-2012

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/AvtarResumeForm.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.forms;
+
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+/**
+ *
+ * @author Sudarsan
+ */
+public class AvtarResumeForm {
+    
+    private String enrollmentNumber;
+    private CommonsMultipartFile resumeDoc;
+
+    public String getEnrollmentNumber() {
+        return enrollmentNumber;
+    }
+
+    public void setEnrollmentNumber(String enrollmentNumber) {
+        this.enrollmentNumber = enrollmentNumber;
+    }
+
+    public CommonsMultipartFile getResumeDoc() {
+        return resumeDoc;
+    }
+
+    public void setResumeDoc(CommonsMultipartFile resumeDoc) {
+        this.resumeDoc = resumeDoc;
+    }
+}
