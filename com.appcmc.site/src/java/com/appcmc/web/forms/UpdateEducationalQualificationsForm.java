f8e877bbfc7b79b7947816ff9a62c14b8633d2f4
hasan143anwar <hasan143anwar@gmail.com>
2012-08-29 10:53:48 +0000

    Added new UpdateEducationalQualificationsForm by Hasan on 29-08-2012

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/UpdateEducationalQualificationsForm.java
@@ -0,0 +1,203 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.forms;
+
+/**
+ *
+ * @author HOME
+ */
+public class UpdateEducationalQualificationsForm {
+    
+    private String enrollmentNumber;
+    private String oneQualification;
+    private String oneYearOfPass;
+    private String oneUniversity;
+    private String oneGrade;
+    private String twoQualification;
+    private String twoSpecialization;
+    private String twoYearOfPass;
+    private String twoUniversity;
+    private String twoGrade;
+    private String threeQualification;
+    private String threeSpecialization;
+    private String threeYearOfPass;
+    private String threeUniversity;
+    private String threeGrade;
+    private String fourQualification;
+    private String fourSpecialization;
+    private String fourYearOfPass;
+    private String fourUniversity;
+    private String fourGrade;
+    private String additionalQualification;
+
+    public String getEnrollmentNumber() {
+        return enrollmentNumber;
+    }
+
+    public void setEnrollmentNumber(String enrollmentNumber) {
+        this.enrollmentNumber = enrollmentNumber;
+    }
+    
+    public String getOneQualification() {
+        return oneQualification;
+    }
+
+    public void setOneQualification(String oneQualification) {
+        this.oneQualification = oneQualification;
+    }
+
+    public String getOneYearOfPass() {
+        return oneYearOfPass;
+    }
+
+    public void setOneYearOfPass(String oneYearOfPass) {
+        this.oneYearOfPass = oneYearOfPass;
+    }
+
+    public String getOneUniversity() {
+        return oneUniversity;
+    }
+
+    public void setOneUniversity(String oneUniversity) {
+        this.oneUniversity = oneUniversity;
+    }
+
+    public String getOneGrade() {
+        return oneGrade;
+    }
+
+    public void setOneGrade(String oneGrade) {
+        this.oneGrade = oneGrade;
+    }
+
+    public String getTwoQualification() {
+        return twoQualification;
+    }
+
+    public void setTwoQualification(String twoQualification) {
+        this.twoQualification = twoQualification;
+    }
+
+    public String getTwoSpecialization() {
+        return twoSpecialization;
+    }
+
+    public void setTwoSpecialization(String twoSpecialization) {
+        this.twoSpecialization = twoSpecialization;
+    }
+
+    public String getTwoYearOfPass() {
+        return twoYearOfPass;
+    }
+
+    public void setTwoYearOfPass(String twoYearOfPass) {
+        this.twoYearOfPass = twoYearOfPass;
+    }
+
+    public String getTwoUniversity() {
+        return twoUniversity;
+    }
+
+    public void setTwoUniversity(String twoUniversity) {
+        this.twoUniversity = twoUniversity;
+    }
+
+    public String getTwoGrade() {
+        return twoGrade;
+    }
+
+    public void setTwoGrade(String twoGrade) {
+        this.twoGrade = twoGrade;
+    }
+
+    public String getThreeQualification() {
+        return threeQualification;
+    }
+
+    public void setThreeQualification(String threeQualification) {
+        this.threeQualification = threeQualification;
+    }
+
+    public String getThreeSpecialization() {
+        return threeSpecialization;
+    }
+
+    public void setThreeSpecialization(String threeSpecialization) {
+        this.threeSpecialization = threeSpecialization;
+    }
+
+    public String getThreeYearOfPass() {
+        return threeYearOfPass;
+    }
+
+    public void setThreeYearOfPass(String threeYearOfPass) {
+        this.threeYearOfPass = threeYearOfPass;
+    }
+
+    public String getThreeUniversity() {
+        return threeUniversity;
+    }
+
+    public void setThreeUniversity(String threeUniversity) {
+        this.threeUniversity = threeUniversity;
+    }
+
+    public String getThreeGrade() {
+        return threeGrade;
+    }
+
+    public void setThreeGrade(String threeGrade) {
+        this.threeGrade = threeGrade;
+    }
+
+    public String getFourQualification() {
+        return fourQualification;
+    }
+
+    public void setFourQualification(String fourQualification) {
+        this.fourQualification = fourQualification;
+    }
+
+    public String getFourSpecialization() {
+        return fourSpecialization;
+    }
+
+    public void setFourSpecialization(String fourSpecialization) {
+        this.fourSpecialization = fourSpecialization;
+    }
+
+    public String getFourYearOfPass() {
+        return fourYearOfPass;
+    }
+
+    public void setFourYearOfPass(String fourYearOfPass) {
+        this.fourYearOfPass = fourYearOfPass;
+    }
+
+    public String getFourUniversity() {
+        return fourUniversity;
+    }
+
+    public void setFourUniversity(String fourUniversity) {
+        this.fourUniversity = fourUniversity;
+    }
+
+    public String getFourGrade() {
+        return fourGrade;
+    }
+
+    public void setFourGrade(String fourGrade) {
+        this.fourGrade = fourGrade;
+    }
+
+    public String getAdditionalQualification() {
+        return additionalQualification;
+    }
+
+    public void setAdditionalQualification(String additionalQualification) {
+        this.additionalQualification = additionalQualification;
+    }
+    
+}
