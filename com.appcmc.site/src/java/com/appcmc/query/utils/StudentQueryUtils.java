c75f5b81000ea09e6243c3105c2afa3b596ca6f8
bhaskar.gedala <bhaskar.gedala@gmail.com>
2012-08-29 07:36:22 +0000

    done nothing by bhaskar on 29-08-2012

--- com.appcmc.site/src/java/com/appcmc/query/utils/StudentQueryUtils.java
+++ com.appcmc.site/src/java/com/appcmc/query/utils/StudentQueryUtils.java
@@ -5,5 +5,4 @@ public interface StudentQueryUtils {
 	public static final String GETALL = "from Student student";
 	public static final String FIND_STUDENT_BY_ENROLLMENTNUBER = "from Student student where student.enrollmentNumber=:EnrollmentNumber";
 	public static final String FIND_STUDENT_BY_STUDENTGUID = "from Student student where student.guid =:Guid";
-
 }
