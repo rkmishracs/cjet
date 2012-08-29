cc82bc91442eb96f32470478d8fad1416ec52582
kishore0522@gmail.com <kishore0522@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-08-29 12:19:05 +0000

    Created A controller/ProfileViewController.java by Kishore on 29/08/2012

---
+++ trunk/com.appcmc.site/src/java/com/appcmc/web/controller/ProfileViewController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author cmc
+ */
+@Controller
+@RequestMapping("/profileView")
+public class ProfileViewController {
+    
+    @RequestMapping(method= RequestMethod.GET)
+    public String showProfileView(){
+        
+        return "/avtar/profileView";
+    }
+        
+    
+}
