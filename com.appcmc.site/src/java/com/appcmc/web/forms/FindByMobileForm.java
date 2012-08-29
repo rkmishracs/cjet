f07b9db8637cd0f66aee692c2f396b8440952ac2
bhaskar.gedala <bhaskar.gedala@gmail.com>
2012-08-29 07:45:16 +0000

    added FindByMobileForm.java to forms by bhaskar on 29-08-2012

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/FindByMobileForm.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.forms;
+
+/**
+ *
+ * @author cmc
+ */
+public class FindByMobileForm {
+    private String mobile;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    
+    
+}
