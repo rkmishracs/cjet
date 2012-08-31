979902b8a7d49749096b2e41e39e6ce5ff07bea6
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-08-31 12:54:07 +0000

    added sendResume method to AppMailService.java by bhaskar on 31-08-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/service/AppMailService.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/service/AppMailService.java
@@ -5,6 +5,8 @@
 package com.appcmc.service;
 
 import com.appcmc.domain.sub.AppUser;
+import com.appcmc.web.forms.EmailProfileForm;
+import java.io.IOException;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.mail.javamail.JavaMailSender;
 
@@ -19,5 +21,7 @@ public interface AppMailService {
     void setVelocityEngine(VelocityEngine velocityEngine);
     
     void sendMail(final AppUser appUser);
+    
+    void sendResume(final EmailProfileForm emailProfileForm);
     
 }
