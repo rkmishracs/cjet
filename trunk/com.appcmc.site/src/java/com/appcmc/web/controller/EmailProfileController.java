4271476314865e34b359786a2e5e09243ac2562c
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-08-31 12:52:59 +0000

    modified EmailProfileController.java to send email by student by bhaskar on 31-08-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/web/controller/EmailProfileController.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/web/controller/EmailProfileController.java
@@ -4,7 +4,14 @@
  */
 package com.appcmc.web.controller;
 
+import com.appcmc.context.id.names.ContextIdNames;
+import com.appcmc.service.AppMailService;
+import com.appcmc.utils.AppContext;
+import com.appcmc.web.forms.EmailProfileForm;
+import java.io.IOException;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,8 +23,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/emailProfile")
 public class EmailProfileController {
     
+    private AppMailService appMailService = null;
+    private static Logger LOG = Logger.getLogger(EmailProfileController.class);
+    
     @RequestMapping(method= RequestMethod.GET)
-    public String emailProfileView(){
+    public String emailProfileView(@ModelAttribute EmailProfileForm emailProfileForm){
+        
+        return "/avtar/emailProfileView";
+    }
+    
+    @RequestMapping(method= RequestMethod.POST, value="/sendEmail")
+    public String getEmailProfile(@ModelAttribute EmailProfileForm emailProfileForm){
+        String emailTo = emailProfileForm.getEmailTo();
+        String emailFrom = emailProfileForm.getEmailFrom();
+        String emailSubject = emailProfileForm.getEmailSubject();
+        String fileName = emailProfileForm.getEmailAttachment().getOriginalFilename();
+        String emailMessage = emailProfileForm.getEmailMessage();
+        
+        LOG.debug("Email To : "+emailTo);
+        LOG.debug("Email From : "+emailFrom);
+        LOG.debug("Email Subject : "+emailSubject);
+        LOG.debug("Email FileName : "+fileName);
+        LOG.debug("Email Message : "+emailMessage);
+        
+         appMailService = (AppMailService) AppContext.APPCONTEXT.getBean(ContextIdNames.APP_MAIL_SERVICE);
+
+        appMailService.sendResume(emailProfileForm);
+        
         
         return "/avtar/emailProfileView";
     }
