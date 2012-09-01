383a7f964c671e81477f1e2fdd02ee375792fb1b
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-01 11:32:04 +0000

    modified SignInForm.java to add rememberMe field by bhaskar on 01-09-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/web/forms/SignInForm.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/web/forms/SignInForm.java
@@ -1,31 +1,39 @@
 /**
- * 
+ *
  */
 package com.appcmc.web.forms;
 
 /**
  * @author Sudarsan
- * 
+ *
  */
 public class SignInForm {
 
-	private String userId;
-	private String password;
+    private String userId;
+    private String password;
+    private String rememberMe;
 
-	public String getUserId() {
-		return userId;
-	}
+    public String getRememberMe() {
+        return rememberMe;
+    }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+    public void setRememberMe(String rememberMe) {
+        this.rememberMe = rememberMe;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getUserId() {
+        return userId;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
