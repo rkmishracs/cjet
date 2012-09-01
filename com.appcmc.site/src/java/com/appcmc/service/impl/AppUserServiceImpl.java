18ca6e5483031e693c199045b60afcfdc7748c8e
bhaskar.gedala <bhaskar.gedala@gmail.com>
2012-09-01 11:35:04 +0000

    modified AppUserServiceImpl.java to add implementation for findByGuid method by bhaskar on 01-09-2012

--- com.appcmc.site/src/java/com/appcmc/service/impl/AppUserServiceImpl.java
+++ com.appcmc.site/src/java/com/appcmc/service/impl/AppUserServiceImpl.java
@@ -15,140 +15,154 @@ import com.appcmc.utils.HibernateUtils;
 
 /**
  * @author GANESH
- * 
+ *
  */
 public class AppUserServiceImpl implements AppUserService {
 
-	private static Logger LOG = Logger.getLogger(AppUserServiceImpl.class);
-	private static List<AppUser> appUserList = new ArrayList<AppUser>();
+    private static Logger LOG = Logger.getLogger(AppUserServiceImpl.class);
+    private static List<AppUser> appUserList = new ArrayList<AppUser>();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.appcmc.service.AppUserService#authenticate(java.lang.String,
-	 * java.lang.String) To authenticate App User in the Application
-	 */
-	@Override
-	public AppUser authenticate(String enrollmentNumber, String password) {
-		AppUser appUser = null;
-		Session session = HibernateUtils.currentSession();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.appcmc.service.AppUserService#authenticate(java.lang.String,
+     * java.lang.String) To authenticate App User in the Application
+     */
+    @Override
+    public AppUser authenticate(String enrollmentNumber, String password) {
+        AppUser appUser = null;
+        Session session = HibernateUtils.currentSession();
 
-		try {
+        try {
 
-			Query query = session.createQuery(AppUserQueryUtils.AUTHENTICATE);
-			query.setString("EnrollmentNumber", enrollmentNumber);
-			query.setString("Password", password);
-			appUser = (AppUser) query.uniqueResult();
-			if (appUser == null) {
-				return appUser;
-			}
-		} catch (Exception exception) {
-			LOG.warn(exception.toString());
+            Query query = session.createQuery(AppUserQueryUtils.AUTHENTICATE);
+            query.setString("EnrollmentNumber", enrollmentNumber);
+            query.setString("Password", password);
+            appUser = (AppUser) query.uniqueResult();
+            if (appUser == null) {
+                return appUser;
+            }
+        } catch (Exception exception) {
+            LOG.warn(exception.toString());
 
-		} finally {
-			HibernateUtils.closeSession();
-		}
-		return appUser;
+        } finally {
+            HibernateUtils.closeSession();
+        }
+        return appUser;
 
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.appcmc.service.AppUserService#getAll() To get the All App Users
-	 * in the Application
-	 */
-	@SuppressWarnings("unchecked")
-	public List<AppUser> getAll() {
-		try {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.appcmc.service.AppUserService#getAll() To get the All App Users
+     * in the Application
+     */
+    @SuppressWarnings("unchecked")
+    public List<AppUser> getAll() {
+        try {
 
-			Session session = HibernateUtils.currentSession();
-			Query query = session.createQuery(AppUserQueryUtils.GETALL);
-			appUserList = (List<AppUser>) query.list();
-		} catch (Exception exception) {
-			LOG.warn(exception.toString());
+            Session session = HibernateUtils.currentSession();
+            Query query = session.createQuery(AppUserQueryUtils.GETALL);
+            appUserList = (List<AppUser>) query.list();
+        } catch (Exception exception) {
+            LOG.warn(exception.toString());
 
-		} finally {
-			HibernateUtils.closeSession();
-		}
-		return appUserList;
+        } finally {
+            HibernateUtils.closeSession();
+        }
+        return appUserList;
 
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.appcmc.service.AppUserService#findByUserEmail(java.lang.String)
-	 * To get the App User By email in the Application
-	 */
-	@Override
-	public AppUser findByUserEmail(String email) {
-		// TODO Auto-generated method stud
-		AppUser appUser = null;
-		try {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.appcmc.service.AppUserService#findByUserEmail(java.lang.String)
+     * To get the App User By email in the Application
+     */
+    @Override
+    public AppUser findByUserEmail(String email) {
+        // TODO Auto-generated method stud
+        AppUser appUser = null;
+        try {
 
-			Session session = HibernateUtils.currentSession();
-			Query query = session.createQuery(AppUserQueryUtils.FINDBYEMAIL);
-			query.setString("Email", email);
-			appUser = (AppUser) query.uniqueResult();
-			if (appUser == null) {
-				return appUser;
-			}
-		} catch (Exception exception) {
-			// TO DO PrintStack Trace with Log4J
-			LOG.warn(exception.toString());
-		} finally {
-			HibernateUtils.closeSession();
-		}
-		return appUser;
-	}
+            Session session = HibernateUtils.currentSession();
+            Query query = session.createQuery(AppUserQueryUtils.FINDBYEMAIL);
+            query.setString("Email", email);
+            appUser = (AppUser) query.uniqueResult();
+            if (appUser == null) {
+                return appUser;
+            }
+        } catch (Exception exception) {
+            // TO DO PrintStack Trace with Log4J
+            LOG.warn(exception.toString());
+        } finally {
+            HibernateUtils.closeSession();
+        }
+        return appUser;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.appcmc.service.AppUserService#create(com.appcmc.domain.sub.AppUser)
-	 * To create an App User in the Application
-	 */
-	public void create(AppUser appUser) {
-		Session session = HibernateUtils.currentSession();
-		Transaction tx = null;
-		boolean rollback = true;
-		try {
-			tx = session.beginTransaction();
-			session.saveOrUpdate(appUser);
-			tx.commit();
-			rollback = false;
-		} catch (Exception exception) {
-			LOG.warn("Exception", exception);
-			HibernateUtils.closeSession();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.appcmc.service.AppUserService#create(com.appcmc.domain.sub.AppUser)
+     * To create an App User in the Application
+     */
+    public void create(AppUser appUser) {
+        Session session = HibernateUtils.currentSession();
+        Transaction tx = null;
+        boolean rollback = true;
+        try {
+            tx = session.beginTransaction();
+            session.saveOrUpdate(appUser);
+            tx.commit();
+            rollback = false;
+        } catch (Exception exception) {
+            LOG.warn("Exception", exception);
 
-		} finally {
-			if (rollback && tx != null) {
-				tx.rollback();
-			}
-			HibernateUtils.closeSession();
-		}
-	}
+        } finally {
+            if (rollback && tx != null) {
+                tx.rollback();
+            }
+            HibernateUtils.closeSession();
+        }
+    }
 
-	@Override
-	public AppUser findByEnrollmentNumber(String enrollmentNumber) {
+    @Override
+    public AppUser findByEnrollmentNumber(String enrollmentNumber) {
 
-		Session session = HibernateUtils.currentSession();
-		AppUser appUser = null;
-		try {
-			Query query = session
-					.createQuery(AppUserQueryUtils.FIND_BY_ENROLLMENT_NUMBER);
-			query.setParameter("EnrollmentNumber", enrollmentNumber);
-			appUser = (AppUser) query.uniqueResult();
-		} catch (Exception exception) {
-			LOG.warn("APPUSER SERVICE", exception);
-			HibernateUtils.closeSession();
-		} finally {
-			HibernateUtils.closeSession();
-		}
+        Session session = HibernateUtils.currentSession();
+        AppUser appUser = null;
+        try {
+            Query query = session
+                    .createQuery(AppUserQueryUtils.FIND_BY_ENROLLMENT_NUMBER);
+            query.setParameter("EnrollmentNumber", enrollmentNumber);
+            appUser = (AppUser) query.uniqueResult();
+        } catch (Exception exception) {
+            LOG.warn("APPUSER SERVICE", exception);
+        } finally {
+            HibernateUtils.closeSession();
+        }
 
-		return appUser;
-	}
+        return appUser;
+    }
 
+    @Override
+    public AppUser findByGuid(String guid) {
+        Session session = HibernateUtils.currentSession();
+        AppUser appUser = null;
+        try {
+            Query query = session.createQuery(AppUserQueryUtils.FIND_BY_GUID);
+            query.setParameter("Guid", guid);
+            appUser = (AppUser) query.uniqueResult();
+        } catch (Exception exception) {
+            LOG.warn("APPUSER SERVICE", exception);
+        } finally {
+            HibernateUtils.closeSession();
+        }
+
+        return appUser;
+    }
 }
