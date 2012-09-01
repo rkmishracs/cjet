9c1a7d04e0938cece0ea914281a2f1167b4952e6
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-01 11:36:50 +0000

    modified AppUserQueryUtils.java to add FIND_BY_GUID static fiels by bhaskar on 01-09-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/query/utils/AppUserQueryUtils.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/query/utils/AppUserQueryUtils.java
@@ -6,5 +6,5 @@ public interface AppUserQueryUtils {
 	public static final String GETALL = "from AppUser appUser";
 	public static final String FINDBYEMAIL = "from AppUser appUser where appUser.email=:Email";
 	public static final String FIND_BY_ENROLLMENT_NUMBER = "from AppUser appUser where appUser.enrollmentNumber=:EnrollmentNumber";
-
+        public static final String FIND_BY_GUID = "from AppUser appUser where appUser.guid =:Guid";
 }
