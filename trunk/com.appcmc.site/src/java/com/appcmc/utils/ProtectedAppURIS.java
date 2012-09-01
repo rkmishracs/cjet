341711de7444bca5d85d35a473580f11217027ef
hasan143anwar@gmail.com <hasan143anwar@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-01 12:15:32 +0000

    Added new strings for security by Hasan on 1-09-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/utils/ProtectedAppURIS.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/utils/ProtectedAppURIS.java
@@ -11,6 +11,17 @@ package com.appcmc.utils;
 public interface ProtectedAppURIS {
     
     public static String APP_USER_MASTER = "/master";
+    public static String APP_USER_MASTER_ENROLLMENT = "/master/showEnrollment";
+    public static String App_USER_MASTER_VIEW_ALL_PROFILES ="/reports/viewAllProfiles";
+    public static String APP_USER_MASTER_VIEW_ALL_MONTHLY_REPORTS = "/reports/monthlyReport";
+    public static String APP_USER_MASTER_VIEW_ALL_WEEKLY_REPORTS = "/reports/weeklyReport";
+    public static String APP_USER_MASTER_VIEW_ALL_DAILY_REPORTS = "/reports/dailyReport";
+    public static String APP_USER_MASTER_SEARCH = "/search";
+    public static String APP_USER_MASTER_VIEW_ALL_ENROLLS = "/reports/all";
     public static String APP_USER_AVTAR = "/avtar";
     public static String APP_USER_AVTAR_UPDATE_PROFILE = "/avtar/update-profile";
+    public static String APP_USER_AVTAR_UPLOAD_RESUME = "/resume";
+    public static String APP_USER_AVTAR_VIEW_RESUME = "/resume/view";
+    public static String APP_USER_AVTAR_EMAIL_RESUME = "/emailProfile";
+    public static String APP_USER_AVTAR_UPDATE_EDUCATION = "/avtar/eduEdit";
 }
