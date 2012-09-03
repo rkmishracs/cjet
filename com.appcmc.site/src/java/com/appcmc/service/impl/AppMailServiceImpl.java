962a4734a49bb441d2160dad4bd1bfe9e398fd69
zarvis.a <zarvis.a@gmail.com>
2012-09-03 16:19:21 +0000

    Added attachment Service to the mailSender by Sudarsan on 03-09-2012

--- com.appcmc.site/src/java/com/appcmc/service/impl/AppMailServiceImpl.java
+++ com.appcmc.site/src/java/com/appcmc/service/impl/AppMailServiceImpl.java
@@ -10,15 +10,18 @@ import com.appcmc.service.AppMailService;
 import com.appcmc.utils.AppContext;
 import com.appcmc.web.controller.EmailProfileController;
 import com.appcmc.web.forms.EmailProfileForm;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import javax.activation.DataSource;
 import javax.mail.internet.MimeMessage;
+import javax.mail.util.ByteArrayDataSource;
 import org.apache.log4j.Logger;
 import org.apache.velocity.app.VelocityEngine;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
@@ -29,104 +32,85 @@ import org.springframework.ui.velocity.VelocityEngineUtils;
  * @author Sudarsan
  */
 public class AppMailServiceImpl implements AppMailService {
-    private static Logger LOG = Logger.getLogger(EmailProfileController.class);
-    private JavaMailSender mailSender;
-    private VelocityEngine velocityEngine;
-    private OutputStream os = null;
-
-    @Override
-    public void setMailSender(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    @Override
-    public void setVelocityEngine(VelocityEngine velocityEngine) {
-        this.velocityEngine = velocityEngine;
-    }
-
-    
-    
-    
-    @Override
-    public void sendMail(final AppUser appUser) {
-        MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
-            @Override
-            public void prepare(MimeMessage mimeMessage) throws Exception {
-
-                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-                mimeMessageHelper.setTo(appUser.getEmail());
-                mimeMessageHelper.setFrom("zarvis.a@gmail.com");
-                Map model = new HashMap();
-                model.put("user", appUser);
-                String text = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "/mailTemplate.vm", model);
-                mimeMessageHelper.setText(text, true);
-
-               
-            }
-        };
-
-        this.mailSender.send(mimeMessagePreparator);
-    }
-    
-    
-     @Override
-    public void sendPassword(final AppUser appUser) {
-        MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
-            @Override
-            public void prepare(MimeMessage mimeMessage) throws Exception {
-
-                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-                mimeMessageHelper.setTo(appUser.getEmail());
-                mimeMessageHelper.setFrom("cmcavtar@gmail.com");
-                Map model = new HashMap();
-                model.put("user", appUser);
-                mimeMessageHelper.setText("Your Password is:"+appUser.getPassword(),true);
-
-                
-
-            }
-        };
-
-        this.mailSender.send(mimeMessagePreparator);
-    }
-
-    @Override
-    public void sendResume(final EmailProfileForm emailProfileForm){
-        MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
-            @Override
-            public void prepare(MimeMessage mimeMessage) throws Exception {
-
-                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED);
-                
-                mimeMessageHelper.setTo(emailProfileForm.getEmailTo());
-                mimeMessageHelper.setFrom(emailProfileForm.getEmailFrom());
-                mimeMessage.setSubject(emailProfileForm.getEmailSubject());
-                Date sentDate = (Date) AppContext.APPCONTEXT.getBean(ContextIdNames.DATE);
-                mimeMessage.setSentDate(sentDate);
-
-                File file = new File(emailProfileForm.getEmailAttachment().getOriginalFilename());
-                byte[] bytes = emailProfileForm.getEmailAttachment().getBytes();
-                LOG.debug(bytes.length);
-                
-                LOG.debug(file.getAbsolutePath());
-                try{
-                    os = new FileOutputStream(file);
-                    os.write(bytes);
-                    LOG.debug(file.length());
-                }catch(Exception exception){
-                    LOG.warn("exception", exception);
-                }finally{
-                    if(os != null){
-                        os.close();
-                    }
-                }
-                
-
-            }
-        };
-
-        this.mailSender.send(mimeMessagePreparator);
-
-
-    }
+   
+   private static Logger LOG = Logger.getLogger(EmailProfileController.class);
+   private JavaMailSender mailSender;
+   private VelocityEngine velocityEngine;
+   private OutputStream os = null;
+   
+   @Override
+   public void setMailSender(JavaMailSender mailSender) {
+      this.mailSender = mailSender;
+   }
+   
+   @Override
+   public void setVelocityEngine(VelocityEngine velocityEngine) {
+      this.velocityEngine = velocityEngine;
+   }
+   
+   @Override
+   public void sendMail(final AppUser appUser) {
+      MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
+         @Override
+         public void prepare(MimeMessage mimeMessage) throws Exception {
+            
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setTo(appUser.getEmail());
+            mimeMessageHelper.setFrom("zarvis.a@gmail.com");
+            Map model = new HashMap();
+            model.put("user", appUser);
+            String text = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "/mailTemplate.vm", model);
+            mimeMessageHelper.setText(text, true);
+            
+            
+         }
+      };
+      
+      this.mailSender.send(mimeMessagePreparator);
+   }
+   
+   @Override
+   public void sendPassword(final AppUser appUser) {
+      MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
+         @Override
+         public void prepare(MimeMessage mimeMessage) throws Exception {
+            
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setTo(appUser.getEmail());
+            mimeMessageHelper.setFrom("cmcavtar@gmail.com");
+            Map model = new HashMap();
+            model.put("user", appUser);
+            mimeMessageHelper.setText("Your Password is:" + appUser.getPassword(), true);
+            
+            
+            
+         }
+      };
+      
+      this.mailSender.send(mimeMessagePreparator);
+   }
+   
+   @Override
+   public void sendResume(final EmailProfileForm emailProfileForm) {
+      MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
+         @Override
+         public void prepare(MimeMessage mimeMessage) throws Exception {
+            
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED);
+            
+            mimeMessageHelper.setTo(emailProfileForm.getEmailTo());
+            mimeMessageHelper.setFrom(emailProfileForm.getEmailFrom());
+            mimeMessage.setSubject(emailProfileForm.getEmailSubject());
+            Date sentDate = (Date) AppContext.APPCONTEXT.getBean(ContextIdNames.DATE);
+            mimeMessage.setSentDate(sentDate);
+            
+            DataSource dataSource = new ByteArrayDataSource(emailProfileForm.getEmailAttachment().getBytes(), emailProfileForm.getEmailAttachment().getContentType());
+            mimeMessageHelper.addInline(emailProfileForm.getEmailAttachment().getName(), dataSource);
+            mimeMessageHelper.addAttachment(emailProfileForm.getEmailAttachment().getOriginalFilename(), dataSource);         }
+      };
+      
+      this.mailSender.send(mimeMessagePreparator);
+      
+      
+   }
 }
