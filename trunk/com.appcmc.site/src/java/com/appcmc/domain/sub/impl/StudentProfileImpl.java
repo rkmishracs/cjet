9be265d18c1cb451f13988d370569541d005705e
zarvis.a@gmail.com <zarvis.a@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-03 03:41:30 +0000

    Implemented StudentProfileImpl.java for the Domain by Sudarsan on 02-09-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/domain/sub/impl/StudentProfileImpl.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/domain/sub/impl/StudentProfileImpl.java
@@ -14,193 +14,166 @@ import java.util.UUID;
  */
 public class StudentProfileImpl implements StudentProfile {
 
-    private Long id;
-    private String guid;
-    private String enrollmentNumber;
-    private String title;
-    private String totalExperience;
-    private String keySkills;
-    private String currentEmployer;
-    private String role;
-    private String previousEmployer;
-    private byte[] resume;
-    private Date createdOn;
-    private Long createdBy;
-    private Date modifiedOn;
-    private Long modifiedBy;
-    private Short active;
-    
-    public StudentProfileImpl(){
-        setGuid(UUID.randomUUID().toString());
-    }
+   private Long id;
+   private String guid;
+   private String enrollmentNumber;
+   private String title;
+   private String totalExperience;
+   private String keySkills;
+   private String currentEmployer;
+   private String role;
+   private String previousEmployer;
+   private byte[] resume;
+   private String fileName;
+   private String type;
+   private Date createdOn;
+   private Long createdBy;
+   private Date modifiedOn;
+   private Long modifiedBy;
+   private Short active;
 
-    public Long getId() {
-        return id;
-    }
+   public StudentProfileImpl() {
+      setGuid(UUID.randomUUID().toString());
+   }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+   public Long getId() {
+      return id;
+   }
 
-    public String getGuid() {
-        return guid;
-    }
+   public void setId(Long id) {
+      this.id = id;
+   }
 
-    public void setGuid(String guid) {
-        this.guid = guid;
-    }
+   public String getGuid() {
+      return guid;
+   }
 
-    public String getEnrollmentNumber() {
-        return enrollmentNumber;
-    }
+   public void setGuid(String guid) {
+      this.guid = guid;
+   }
 
-    public void setEnrollmentNumber(String enrollmentNumber) {
-        this.enrollmentNumber = enrollmentNumber;
-    }
+   public String getEnrollmentNumber() {
+      return enrollmentNumber;
+   }
 
-    public String getTitle() {
-        return title;
-    }
+   public void setEnrollmentNumber(String enrollmentNumber) {
+      this.enrollmentNumber = enrollmentNumber;
+   }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+   public String getTitle() {
+      return title;
+   }
 
-    public String getTotalExperience() {
-        return totalExperience;
-    }
+   public void setTitle(String title) {
+      this.title = title;
+   }
 
-    public void setTotalExperience(String totalExperience) {
-        this.totalExperience = totalExperience;
-    }
+   public String getTotalExperience() {
+      return totalExperience;
+   }
 
-    public String getKeySkills() {
-        return keySkills;
-    }
+   public void setTotalExperience(String totalExperience) {
+      this.totalExperience = totalExperience;
+   }
 
-    public void setKeySkills(String keySkills) {
-        this.keySkills = keySkills;
-    }
+   public String getKeySkills() {
+      return keySkills;
+   }
 
-    public String getCurrentEmployer() {
-        return currentEmployer;
-    }
+   public void setKeySkills(String keySkills) {
+      this.keySkills = keySkills;
+   }
 
-    public void setCurrentEmployer(String currentEmployer) {
-        this.currentEmployer = currentEmployer;
-    }
+   public String getCurrentEmployer() {
+      return currentEmployer;
+   }
 
-    public String getRole() {
-        return role;
-    }
+   public void setCurrentEmployer(String currentEmployer) {
+      this.currentEmployer = currentEmployer;
+   }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+   public String getRole() {
+      return role;
+   }
 
-    public String getPreviousEmployer() {
-        return previousEmployer;
-    }
+   public void setRole(String role) {
+      this.role = role;
+   }
 
-    public void setPreviousEmployer(String previousEmployer) {
-        this.previousEmployer = previousEmployer;
-    }
+   public String getPreviousEmployer() {
+      return previousEmployer;
+   }
 
-    public byte[] getResume() {
-        return resume;
-    }
+   public void setPreviousEmployer(String previousEmployer) {
+      this.previousEmployer = previousEmployer;
+   }
 
-    public void setResume(byte[] resume) {
-        this.resume = resume;
-    }
+   public byte[] getResume() {
+      return resume;
+   }
 
-    public Date getCreatedOn() {
-        return createdOn;
-    }
+   public void setResume(byte[] resume) {
+      this.resume = resume;
+   }
 
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
+   public String getFileName() {
+      return fileName;
+   }
 
-    public Long getCreatedBy() {
-        return createdBy;
-    }
+   public void setFileName(String fileName) {
+      this.fileName = fileName;
+   }
 
-    public void setCreatedBy(Long createdBy) {
-        this.createdBy = createdBy;
-    }
+   public String getType() {
+      return type;
+   }
 
-    public Date getModifiedOn() {
-        return modifiedOn;
-    }
+   public void setType(String type) {
+      this.type = type;
+   }
 
-    public void setModifiedOn(Date modifiedOn) {
-        this.modifiedOn = modifiedOn;
-    }
+   public Date getCreatedOn() {
+      return createdOn;
+   }
 
-    public Long getModifiedBy() {
-        return modifiedBy;
-    }
+   public void setCreatedOn(Date createdOn) {
+      this.createdOn = createdOn;
+   }
 
-    public void setModifiedBy(Long modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
+   public Long getCreatedBy() {
+      return createdBy;
+   }
 
-    public Short getActive() {
-        return active;
-    }
+   public void setCreatedBy(Long createdBy) {
+      this.createdBy = createdBy;
+   }
 
-    public void setActive(Short active) {
-        this.active = active;
-    }
+   public Date getModifiedOn() {
+      return modifiedOn;
+   }
 
-    public String toString() {
-        StringBuilder toStringBuilder = new StringBuilder();
-        toStringBuilder.append(super.toString());
-        toStringBuilder.append("\n");
-        toStringBuilder.append("\nid: ");
-        toStringBuilder.append(id);
-        toStringBuilder.append("\nguid: ");
-        toStringBuilder.append(guid);
-        toStringBuilder.append("\nenrollmentNumber: ");
-        toStringBuilder.append(enrollmentNumber);
-        toStringBuilder.append("\ntitle: ");
-        toStringBuilder.append(title);
-        toStringBuilder.append("\ntotalExperience: ");
-        toStringBuilder.append(totalExperience);
-        toStringBuilder.append("\nkeySkills: ");
-        toStringBuilder.append(keySkills);
-        toStringBuilder.append("\ncurrentEmployer: ");
-        toStringBuilder.append(currentEmployer);
-        toStringBuilder.append("\nrole: ");
-        toStringBuilder.append(role);
-        toStringBuilder.append("\npreviousEmployer: ");
-        toStringBuilder.append(previousEmployer);
-        toStringBuilder.append("\nresume: ");
-        if (resume != null) {
-            toStringBuilder.append("\nSize: ");
-            toStringBuilder.append(resume.length);
-            for (int i = 0; i < resume.length; ++i) {
-                toStringBuilder.append("\nIndex ");
-                toStringBuilder.append(i);
-                toStringBuilder.append(": ");
-                toStringBuilder.append(resume[i]);
-            }
-        } else {
-            toStringBuilder.append("NULL");
-        }
-        toStringBuilder.append("\ncreatedOn: ");
-        toStringBuilder.append(createdOn);
-        toStringBuilder.append("\ncreatedBy: ");
-        toStringBuilder.append(createdBy);
-        toStringBuilder.append("\nmodifiedOn: ");
-        toStringBuilder.append(modifiedOn);
-        toStringBuilder.append("\nmodifiedBy: ");
-        toStringBuilder.append(modifiedBy);
-        toStringBuilder.append("\nactive: ");
-        toStringBuilder.append(active);
-        return toStringBuilder.toString();
-    }
+   public void setModifiedOn(Date modifiedOn) {
+      this.modifiedOn = modifiedOn;
+   }
 
-  
+   public Long getModifiedBy() {
+      return modifiedBy;
+   }
+
+   public void setModifiedBy(Long modifiedBy) {
+      this.modifiedBy = modifiedBy;
+   }
+
+   public Short getActive() {
+      return active;
+   }
+
+   public void setActive(Short active) {
+      this.active = active;
+   }
+
+   @Override
+   public String toString() {
+      return "StudentProfileImpl{" + "id=" + id + ", guid=" + guid + ", enrollmentNumber=" + enrollmentNumber + ", title=" + title + ", totalExperience=" + totalExperience + ", keySkills=" + keySkills + ", currentEmployer=" + currentEmployer + ", role=" + role + ", previousEmployer=" + previousEmployer + ", resume=" + resume + ", fileName=" + fileName + ", type=" + type + ", createdOn=" + createdOn + ", createdBy=" + createdBy + ", modifiedOn=" + modifiedOn + ", modifiedBy=" + modifiedBy + ", active=" + active + '}';
+   }
 }
