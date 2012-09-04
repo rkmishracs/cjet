3418e7b709a279c2994af92ab1045cf9a3a148a2
bhaskar.gedala <bhaskar.gedala@gmail.com>
2012-09-04 09:07:21 +0000

    modofied appcmc.sql to add app_user_events table by bhaskar on 04-09-2012

--- com.appcmc.site/src/java/com/appcmc/database/appcmc.sql
+++ com.appcmc.site/src/java/com/appcmc/database/appcmc.sql
@@ -1,24 +1,34 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.0.45-community-nt - MySQL Community Edition (GPL)
--- Server OS:                    Win32
--- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2012-08-27 10:49:51
--- --------------------------------------------------------
+# HeidiSQL Dump 
+#
+# --------------------------------------------------------
+# Host:                 127.0.0.1
+# Database:             app
+# Server version:       5.0.45-community-nt
+# Server OS:            Win32
+# Target-Compatibility: MySQL 5.0
+# max_allowed_packet:   1048576
+# HeidiSQL version:     3.1 RC1 Revision: 1064
+# --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40100 SET CHARACTER SET latin1*/;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0*/;
 
--- Dumping database structure for app
+
+#
+# Database structure for database 'app'
+#
+
 DROP DATABASE IF EXISTS `app`;
-CREATE DATABASE IF NOT EXISTS `app` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE `app` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
 USE `app`;
 
 
--- Dumping structure for table app.app_user
-DROP TABLE IF EXISTS `app_user`;
-CREATE TABLE IF NOT EXISTS `app_user` (
+#
+# Table structure for table 'app_user'
+#
+
+CREATE TABLE `app_user` (
   `usr_uid` bigint(10) unsigned NOT NULL auto_increment,
   `usr_guid` varchar(64) NOT NULL,
   `usr_email` varchar(255) NOT NULL,
@@ -39,18 +49,27 @@ CREATE TABLE IF NOT EXISTS `app_user` (
   `usr_security_answer` varchar(255) default NULL,
   `usr_active` tinyint(1) unsigned NOT NULL default '1',
   PRIMARY KEY  (`usr_uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
-
--- Dumping data for table app.app_user: ~1 rows (approximately)
-/*!40000 ALTER TABLE `app_user` DISABLE KEYS */;
-REPLACE INTO `app_user` (`usr_uid`, `usr_guid`, `usr_email`, `usr_enrollment_number`, `usr_password`, `usr_screen_name`, `usr_first_name`, `usr_last_name`, `usr_status_id`, `usr_gender`, `usr_time_zone`, `usr_type`, `usr_created_on`, `usr_created_by`, `usr_modified_on`, `usr_modified_by`, `usr_security_question`, `usr_security_answer`, `usr_active`) VALUES
-	(1, '1A4C6D9C-0AED-7CCB-C0FA-5F629A5026E3', 'admin@appcmc.com', '999', 'pwd', 'Admin', 'Admin', 'Admin', 1, 'm', '2012-07-10 17:42:15', 'admin', '2012-07-11 12:56:40', 1, '2012-08-13 16:21:24', 1, 'What Town Were You Born In?', 'Vijayawada', 1);
-/*!40000 ALTER TABLE `app_user` ENABLE KEYS */;
+) ENGINE=InnoDB AUTO_INCREMENT=22 /*!40100 DEFAULT CHARSET=latin1*/;
 
 
--- Dumping structure for table app.app_user_contacts
-DROP TABLE IF EXISTS `app_user_contacts`;
-CREATE TABLE IF NOT EXISTS `app_user_contacts` (
+
+#
+# Dumping data for table 'app_user'
+#
+
+/*!40000 ALTER TABLE `app_user` DISABLE KEYS*/;
+LOCK TABLES `app_user` WRITE;
+INSERT INTO `app_user` (`usr_uid`, `usr_guid`, `usr_email`, `usr_enrollment_number`, `usr_password`, `usr_screen_name`, `usr_first_name`, `usr_last_name`, `usr_status_id`, `usr_gender`, `usr_time_zone`, `usr_type`, `usr_created_on`, `usr_created_by`, `usr_modified_on`, `usr_modified_by`, `usr_security_question`, `usr_security_answer`, `usr_active`) VALUES
+	('1','1A4C6D9C-0AED-7CCB-C0FA-5F629A5026E3','admin@appcmc.com','999','pwd','Admin','Admin','Admin','1','m','2012-07-10 17:42:15','admin','2012-07-11 12:56:40','1','2012-08-13 16:21:24','1','What Town Were You Born In?','Vijayawada',1);
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `app_user` ENABLE KEYS*/;
+
+
+#
+# Table structure for table 'app_user_contacts'
+#
+
+CREATE TABLE `app_user_contacts` (
   `contact_uid` bigint(10) unsigned NOT NULL auto_increment,
   `contact_guid` varchar(64) NOT NULL,
   `contact_city` varchar(255) NOT NULL,
@@ -69,16 +88,23 @@ CREATE TABLE IF NOT EXISTS `app_user_contacts` (
   `contact_modified_by` bigint(10) unsigned NOT NULL,
   `contact_active` tinyint(1) unsigned NOT NULL default '1',
   PRIMARY KEY  (`contact_uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-
--- Dumping data for table app.app_user_contacts: ~0 rows (approximately)
-/*!40000 ALTER TABLE `app_user_contacts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `app_user_contacts` ENABLE KEYS */;
+) ENGINE=InnoDB AUTO_INCREMENT=13 /*!40100 DEFAULT CHARSET=latin1*/;
 
 
--- Dumping structure for table app.app_user_educational_qualifications
-DROP TABLE IF EXISTS `app_user_educational_qualifications`;
-CREATE TABLE IF NOT EXISTS `app_user_educational_qualifications` (
+
+#
+# Dumping data for table 'app_user_contacts'
+#
+
+# (No data found.)
+
+
+
+#
+# Table structure for table 'app_user_educational_qualifications'
+#
+
+CREATE TABLE `app_user_educational_qualifications` (
   `edu_uid` bigint(10) unsigned NOT NULL auto_increment,
   `edu_guid` varchar(64) NOT NULL,
   `edu_enrollment_number` varchar(255) NOT NULL,
@@ -108,16 +134,59 @@ CREATE TABLE IF NOT EXISTS `app_user_educational_qualifications` (
   `edu_modified_by` bigint(10) unsigned NOT NULL,
   `edu_active` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY  (`edu_uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
--- Dumping data for table app.app_user_educational_qualifications: ~0 rows (approximately)
-/*!40000 ALTER TABLE `app_user_educational_qualifications` DISABLE KEYS */;
-/*!40000 ALTER TABLE `app_user_educational_qualifications` ENABLE KEYS */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 /*!40100 DEFAULT CHARSET=latin1*/;
 
 
--- Dumping structure for table app.app_user_profile
-DROP TABLE IF EXISTS `app_user_profile`;
-CREATE TABLE IF NOT EXISTS `app_user_profile` (
+
+#
+# Dumping data for table 'app_user_educational_qualifications'
+#
+
+# (No data found.)
+
+
+
+#
+# Table structure for table 'app_user_events'
+#
+
+CREATE TABLE `app_user_events` (
+  `evnt_uid` bigint(20) unsigned NOT NULL auto_increment,
+  `evnt_guid` varchar(255) NOT NULL,
+  `evnt_event_name` varchar(255) default NULL,
+  `evnt_event_type` varchar(255) default NULL,
+  `evnt_event_on` datetime default NULL,
+  `evnt_event_time` varchar(255) default NULL,
+  `evnt_event_location` varchar(255) default NULL,
+  `evnt_additional_info` varchar(255) default NULL,
+  `evnt_created_on` datetime NOT NULL,
+  `evnt_created_by` bigint(10) unsigned NOT NULL,
+  `evnt_modified_on` datetime NOT NULL,
+  `evnt_modified_by` bigint(10) unsigned NOT NULL,
+  `evnt_active` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY  (`evnt_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 /*!40100 DEFAULT CHARSET=latin1*/;
+
+
+
+#
+# Dumping data for table 'app_user_events'
+#
+
+/*!40000 ALTER TABLE `app_user_events` DISABLE KEYS*/;
+LOCK TABLES `app_user_events` WRITE;
+INSERT INTO `app_user_events` (`evnt_uid`, `evnt_guid`, `evnt_event_name`, `evnt_event_type`, `evnt_event_on`, `evnt_event_time`, `evnt_event_location`, `evnt_additional_info`, `evnt_created_on`, `evnt_created_by`, `evnt_modified_on`, `evnt_modified_by`, `evnt_active`) VALUES
+	('1','dbd553aa-3ff9-4924-a402-96cb6bd525f3','Satyam Fest','WalkIn','2012-09-04 14:03:19','9AM','vizag','nothing','2012-09-04 14:03:19','1','2012-09-04 14:03:19','1',1),
+	('2','fc3b7c86-9e75-4e4a-a7dd-c2efc62f9aa6','wipro Job Fest','JobFare','2012-09-04 14:04:32','10AM','Hyd','nothing','2012-09-04 14:04:32','1','2012-09-04 14:04:32','1',1);
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `app_user_events` ENABLE KEYS*/;
+
+
+#
+# Table structure for table 'app_user_profile'
+#
+
+CREATE TABLE `app_user_profile` (
   `profile_uid` bigint(10) unsigned NOT NULL auto_increment,
   `profile_guid` varchar(64) NOT NULL,
   `profile_enrollment_number` varchar(255) NOT NULL,
@@ -128,8 +197,8 @@ CREATE TABLE IF NOT EXISTS `app_user_profile` (
   `profile_role` varchar(255) default NULL,
   `profile_previous_employer` varchar(255) default NULL,
   `profile_resume` longblob,
-  `profile_resume_filename` varchar(50),
-  `profile_resume_type` varchar(50),
+  `profile_resume_filename` varchar(50) default NULL,
+  `profile_resume_type` varchar(50) default NULL,
   `profile_created_on` datetime NOT NULL,
   `profile_created_by` bigint(10) unsigned NOT NULL,
   `profile_modified_on` datetime NOT NULL,
@@ -137,16 +206,23 @@ CREATE TABLE IF NOT EXISTS `app_user_profile` (
   `profile_active` tinyint(1) unsigned NOT NULL,
   `Column 15` int(10) default NULL,
   PRIMARY KEY  (`profile_uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
--- Dumping data for table app.app_user_profile: ~0 rows (approximately)
-/*!40000 ALTER TABLE `app_user_profile` DISABLE KEYS */;
-/*!40000 ALTER TABLE `app_user_profile` ENABLE KEYS */;
+) ENGINE=InnoDB AUTO_INCREMENT=4 /*!40100 DEFAULT CHARSET=latin1*/;
 
 
--- Dumping structure for table app.app_user_student
-DROP TABLE IF EXISTS `app_user_student`;
-CREATE TABLE IF NOT EXISTS `app_user_student` (
+
+#
+# Dumping data for table 'app_user_profile'
+#
+
+# (No data found.)
+
+
+
+#
+# Table structure for table 'app_user_student'
+#
+
+CREATE TABLE `app_user_student` (
   `student_uid` bigint(10) unsigned NOT NULL auto_increment,
   `student_guid` varchar(64) NOT NULL,
   `student_enrollment_number` varchar(255) NOT NULL,
@@ -170,28 +246,14 @@ CREATE TABLE IF NOT EXISTS `app_user_student` (
   `student_active` tinyint(1) unsigned NOT NULL default '1',
   `student_contacts_student` bigint(10) unsigned default NULL,
   PRIMARY KEY  (`student_uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
--- Dumping data for table app.app_user_student: ~0 rows (approximately)
-/*!40000 ALTER TABLE `app_user_student` DISABLE KEYS */;
-/*!40000 ALTER TABLE `app_user_student` ENABLE KEYS */;
+) ENGINE=InnoDB AUTO_INCREMENT=10 /*!40100 DEFAULT CHARSET=latin1*/;
 
 
--- Dumping database structure for testdump
-DROP DATABASE IF EXISTS `testdump`;
-CREATE DATABASE IF NOT EXISTS `testdump` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `testdump`;
 
+#
+# Dumping data for table 'app_user_student'
+#
 
--- Dumping structure for table testdump.student
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE IF NOT EXISTS `student` (
-  `sno` varchar(50) default NULL,
-  `pic` mediumblob
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+# (No data found.)
 
--- Dumping data for table testdump.student: ~0 rows (approximately)
-/*!40000 ALTER TABLE `student` DISABLE KEYS */;
-/*!40000 ALTER TABLE `student` ENABLE KEYS */;
-/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS*/;
