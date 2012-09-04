05f5246cfb165f1679ce91fa2e24d1c98303cf87
kishore0522 <kishore0522@gmail.com>
2012-09-04 11:17:46 +0000

    Added Labels For PLACED_STUDENT and PLACED_STUDENT_SERVICE BY Kishore on 04/09/2012

--- com.appcmc.site/src/java/com/appcmc/context/id/names/ContextIdNames.java
+++ com.appcmc.site/src/java/com/appcmc/context/id/names/ContextIdNames.java
@@ -17,6 +17,7 @@ public interface ContextIdNames {
         public static final String EDUCATIONAL_QUALIFICATIONS = "eduactionalQualifications";
         public static final String MAIL_SENDER= "mailSender";
         public static final String EVENTS = "events";
+        public static final String PLACED_STUDENT = "placedStudent";
 
 	
 	// Services
@@ -26,6 +27,7 @@ public interface ContextIdNames {
         public static final String STUDENT_PROFILE_SERVICE = "studentProfileService";
         public static final String EDUCATIONAL_QUALIFICATIONS_SERVICE = "educationalQualificationService";
         public static final String EVENT_SERVICE = "eventsService";
+        public static final String PLACED_STUDENT_SERVICE = "placedStudentService";
         
         //method
         public static final String SIMPLE_DATE_FORMAT = "simpleDateFormat";
