bd01fba400aef5233ad06acb57fa6876ad127b71
bhaskar.gedala <bhaskar.gedala@gmail.com>
2012-09-04 09:02:32 +0000

    added EventsService.java to service folder by bhaskar on 04-09-2012

---
+++ com.appcmc.site/src/java/com/appcmc/service/EventsService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.service;
+
+import com.appcmc.domain.sub.Events;
+import java.util.List;
+
+/**
+ *
+ * @author cmc
+ */
+public interface EventsService {
+    
+    void create(Events events);
+    
+    List<Events> getAll();
+    
+}
