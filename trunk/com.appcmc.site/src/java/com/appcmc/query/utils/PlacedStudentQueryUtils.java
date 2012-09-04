f16100dfb820bf59370121384efacc24e3aaaf2a
kishore0522@gmail.com <kishore0522@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-04 11:20:59 +0000

    Written Query for PlacedStudent in the name of PlacedStudentQueryUtils BY Kishore on 04/09/2012

---
+++ trunk/com.appcmc.site/src/java/com/appcmc/query/utils/PlacedStudentQueryUtils.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.query.utils;
+
+/**
+ *
+ * @author cmc
+ */
+public interface PlacedStudentQueryUtils {
+    
+   public static final String GETALL = "from PlacedStudent placedStudent";
+}
