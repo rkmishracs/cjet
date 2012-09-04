abc112af5cb9018a1ecb96393f1dab892ea97697
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-04 06:03:04 +0000

    Modified EmailProfileController.java to send success and failure responses by bhaskar on 04-09-2012

--- trunk/com.appcmc.site/src/java/com/appcmc/web/controller/EmailProfileController.java
+++ trunk/com.appcmc.site/src/java/com/appcmc/web/controller/EmailProfileController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
 
 /**
  *
@@ -34,7 +35,7 @@ public class EmailProfileController {
     }
     
     @RequestMapping(method= RequestMethod.POST, value="/sendEmail")
-    public String getEmailProfile(@ModelAttribute EmailProfileForm emailProfileForm){
+    public String getEmailProfile(@ModelAttribute EmailProfileForm emailProfileForm, WebRequest request){
         String emailTo = emailProfileForm.getEmailTo();
         String emailFrom = emailProfileForm.getEmailFrom();
         String emailSubject = emailProfileForm.getEmailSubject();
@@ -47,13 +48,17 @@ public class EmailProfileController {
         LOG.debug("Email FileName : "+fileName);
         LOG.debug("Email Message : "+emailMessage);
         
-        
+        if(!fileName.endsWith(".doc")){
+            request.setAttribute("resumeFailureResponse", "Resume Must '.doc' file", WebRequest.SCOPE_REQUEST);
+            return "/avtar/emailProfileView";
+        }
         
         
          appMailService = (AppMailService) AppContext.APPCONTEXT.getBean(ContextIdNames.APP_MAIL_SERVICE);
 
         appMailService.sendResume(emailProfileForm);
         
+        request.setAttribute("resumeSuccessResponse", "Resume Sent Successfully", WebRequest.SCOPE_REQUEST);
         
         return "/avtar/emailProfileView";
     }
