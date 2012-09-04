b12058ac649d756dbd1178b4f2af9e63d01c277a
hasan143anwar <hasan143anwar@gmail.com>
2012-09-04 11:51:22 +0000

    Added a new form EventCreationFrom.java for creating an event by Hasan on 04-09-2012

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/EventCreationFrom.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.forms;
+
+/**
+ *
+ * @author HOME
+ */
+public class EventCreationFrom {
+    
+    private String eventName;
+    private String eventType;
+    private String eventDate;
+    private String eventTime;
+    private String eventLocation;
+    private String eventAdditionalInformation;
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime = eventTime;
+    }
+    
+    public String getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventLocation(String eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+
+    public String getEventAdditionalInformation() {
+        return eventAdditionalInformation;
+    }
+
+    public void setEventAdditionalInformation(String eventAdditionalInformation) {
+        this.eventAdditionalInformation = eventAdditionalInformation;
+    }
+}
