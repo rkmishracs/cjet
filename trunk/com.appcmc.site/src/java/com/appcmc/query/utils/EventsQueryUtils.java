2d2d80f8266b3e90e306476096409cbebbe185e7
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-04 09:01:33 +0000

    added EventsQueryUtils.java to query/utils folder by bhaskar on 04-09-2012

---
+++ trunk/com.appcmc.site/src/java/com/appcmc/query/utils/EventsQueryUtils.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.query.utils;
+
+/**
+ *
+ * @author cmc
+ */
+public interface EventsQueryUtils {
+    
+    public static final String GET_ALL = "from Events events";
+    
+}
