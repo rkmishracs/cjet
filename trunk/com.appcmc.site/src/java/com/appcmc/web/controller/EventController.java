f89d7e10744416015c0dec65dd8266cac88b4c69
hasan143anwar@gmail.com <hasan143anwar@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-04 11:52:36 +0000

    Added EventController.java for viewing creating of Events by Hasan on 04-09-2012

---
+++ trunk/com.appcmc.site/src/java/com/appcmc/web/controller/EventController.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.controller;
+
+import com.appcmc.context.id.names.ContextIdNames;
+import com.appcmc.domain.sub.AppUser;
+import com.appcmc.domain.sub.Events;
+import com.appcmc.service.EventsService;
+import com.appcmc.utils.AppContext;
+import com.appcmc.web.forms.EventCreationFrom;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ *
+ * @author HOME
+ */
+
+@Controller
+@RequestMapping("/event")
+public class EventController {
+    
+    static Logger LOG = Logger.getLogger(ReportsController.class);
+    private Events events = null;
+    private EventsService eventsService = null;
+    private Date eventdate = null;
+    private Date date  = null;
+    
+    @RequestMapping(method= RequestMethod.GET)
+    public String showEventsPage(){
+        
+        LOG.debug("In Event Controller");
+        
+        return "/master/eventsHome";
+    }
+    
+    @RequestMapping(method= RequestMethod.GET, value="/showCreateEvent")
+    public String showCreateEventPage(@ModelAttribute EventCreationFrom eventCreationFrom){
+        return "/master/createEvent";
+    }
+    
+    
+    @RequestMapping(method= RequestMethod.POST, value="/createEvent")
+    @ResponseBody
+    public String createEvent(@ModelAttribute EventCreationFrom eventCreationFrom, WebRequest request){
+        
+        date = (Date) AppContext.APPCONTEXT.getBean(ContextIdNames.DATE);
+        
+        
+       LOG.debug("In create Event");
+
+        SimpleDateFormat simpleDateFormat = (SimpleDateFormat) AppContext.APPCONTEXT.getBean(ContextIdNames.SIMPLE_DATE_FORMAT);
+        try {
+
+            eventdate = simpleDateFormat.parse(eventCreationFrom.getEventDate());
+        } catch (ParseException parseException) {
+            LOG.debug("exception", parseException);
+        }
+
+        AppUser appUser = (AppUser) request.getAttribute("user", WebRequest.SCOPE_SESSION);
+        
+        LOG.debug(appUser);
+        
+        eventsService = (EventsService) AppContext.APPCONTEXT.getBean(ContextIdNames.EVENT_SERVICE);
+        
+        events = (Events) AppContext.APPCONTEXT.getBean(ContextIdNames.EVENTS);
+        
+        events.setEventName(eventCreationFrom.getEventName());
+        events.setEventType(eventCreationFrom.getEventType());
+        events.setEventOn(eventdate);
+        events.setEventTime(eventCreationFrom.getEventTime());
+        events.setEventLocation(eventCreationFrom.getEventLocation());
+        events.setAdditionalInfo(eventCreationFrom.getEventAdditionalInformation());
+        events.setCreatedOn(date);
+        events.setModifiedOn(date);
+        events.setCreatedBy(appUser.getCreatedBy());
+        events.setModifiedBy(appUser.getModifiedBy());
+        events.setActive(Short.parseShort("1"));
+        
+        eventsService.create(events);
+        
+        return "success";
+    }
+}
