f472df65e0af46036a06106405cd77dadc5eb217
bhaskar.gedala@gmail.com <bhaskar.gedala@gmail.com@5ac5fd9f-6b51-1c5f-3205-85291ee61fa9>
2012-09-04 09:04:11 +0000

    added EventsTest.java to tests folder by bhaskar on 04-09-2012

---
+++ trunk/com.appcmc.site/src/java/com/appcmc/tests/EventsTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.tests;
+
+import com.appcmc.context.id.names.ContextIdNames;
+import com.appcmc.domain.sub.Events;
+import com.appcmc.service.EventsService;
+import com.appcmc.utils.AppCmcSpringContext;
+import com.appcmc.utils.AppContext;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author cmc
+ */
+public class EventsTest {
+
+    private static Logger LOG = Logger.getLogger(EventsTest.class);
+    
+    public static void main(String[] args) {
+        AppCmcSpringContext.init();
+        EventsService eventsService = (EventsService) AppContext.APPCONTEXT.getBean(ContextIdNames.EVENT_SERVICE);
+        
+        /*Events events = (Events) AppContext.APPCONTEXT.getBean(ContextIdNames.EVENTS);
+        
+        events.setEventName("wipro Job Fest");
+        events.setEventType("JobFare");
+        events.setEventOn(new java.util.Date());
+        events.setEventLocation("Hyd");
+        events.setEventTime("10AM");
+        events.setAdditionalInfo("nothing");
+        events.setCreatedOn(new java.util.Date());
+        events.setCreatedBy(1L);
+        events.setModifiedOn(new java.util.Date());
+        events.setModifiedBy(1L);
+        events.setActive(Short.parseShort("1"));
+        
+        eventsService.create(events);*/
+        
+        List<Events> eventsList = eventsService.getAll();
+        for(Events event : eventsList){
+            LOG.debug(event);
+            
+        }
+    }
+}
