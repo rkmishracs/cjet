38c3721653df924006d477b1371c642687084e8a
hasan143anwar <hasan143anwar@gmail.com>
2012-09-06 12:03:04 +0000

    Added new form SuccessStoryCreationForm.java for creation of successStories by Hasan on 06-09-2012

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/SuccessStoryCreationForm.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.forms;
+
+/**
+ *
+ * @author HOME
+ */
+public class SuccessStoryCreationForm {
+    
+    private String sucessStoriesEnrollmentNumber;
+    private String personName;
+    private String placedCompanyName;
+    private String yearOfPlacement;
+    private String designation;
+    private String packageDetails;
+    private String otherDetails;
+
+    public String getSucessStoriesEnrollmentNumber() {
+        return sucessStoriesEnrollmentNumber;
+    }
+
+    public void setSucessStoriesEnrollmentNumber(String sucessStoriesEnrollmentNumber) {
+        this.sucessStoriesEnrollmentNumber = sucessStoriesEnrollmentNumber;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPlacedCompanyName() {
+        return placedCompanyName;
+    }
+
+    public void setPlacedCompanyName(String placedCompanyName) {
+        this.placedCompanyName = placedCompanyName;
+    }
+
+    public String getYearOfPlacement() {
+        return yearOfPlacement;
+    }
+
+    public void setYearOfPlacement(String yearOfPlacement) {
+        this.yearOfPlacement = yearOfPlacement;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getPackageDetails() {
+        return packageDetails;
+    }
+
+    public void setPackageDetails(String packageDetails) {
+        this.packageDetails = packageDetails;
+    }
+
+    public String getOtherDetails() {
+        return otherDetails;
+    }
+
+    public void setOtherDetails(String otherDetails) {
+        this.otherDetails = otherDetails;
+    }
+    
+}
