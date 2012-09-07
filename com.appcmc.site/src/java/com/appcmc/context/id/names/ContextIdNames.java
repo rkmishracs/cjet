74f6d7ce84f8109bbd6e70c5fdaa6fc0e03163d0
zarvis.a <zarvis.a@gmail.com>
2012-09-07 04:33:27 +0000

    Added beans by Sudarsan on 06-09-2012

--- com.appcmc.site/src/java/com/appcmc/context/id/names/ContextIdNames.java
+++ com.appcmc.site/src/java/com/appcmc/context/id/names/ContextIdNames.java
@@ -1,41 +1,40 @@
 /**
- * 
+ *
  */
 package com.appcmc.context.id.names;
+
 /**
  * @author Sudarsan
- * 
+ *
  */
 public interface ContextIdNames {
 
-	// Domains
-	public static final String APP_USER = "appUser";
-	public static final String STUDENT = "student";
-	public static final String CONTACTS = "contacts";
-        public static final String STUDENT_CONTACTS = "studentContacts";        
-        public static final String STUDENT_PROFILE = "studentProfile";
-        public static final String EDUCATIONAL_QUALIFICATIONS = "eduactionalQualifications";
-        public static final String MAIL_SENDER= "mailSender";
-        public static final String EVENTS = "events";
-        public static final String PLACED_STUDENT = "placedStudent";
-
-	
-	// Services
-	public static final String APP_USER_SERVICE = "appUserService";
-	public static final String STUDENT_SERVICE = "studentService";
-	public static final String CONTACT_SERVICE = "contactsService";
-        public static final String STUDENT_PROFILE_SERVICE = "studentProfileService";
-        public static final String EDUCATIONAL_QUALIFICATIONS_SERVICE = "educationalQualificationService";
-        public static final String EVENT_SERVICE = "eventsService";
-        public static final String PLACED_STUDENT_SERVICE = "placedStudentService";
-        
-        //method
-        public static final String SIMPLE_DATE_FORMAT = "simpleDateFormat";
-        public static final String DATE = "date";
-        public static final String SHORT = "short";
-
-
-        public static final String APP_MAIL_SERVICE = "appMailService";
-        public static final String VELOCITY_ENGINE = "velocityEngine";
+   // Domains
+   public static final String APP_USER = "appUser";
+   public static final String STUDENT = "student";
+   public static final String CONTACTS = "contacts";
+   public static final String STUDENT_CONTACTS = "studentContacts";
+   public static final String STUDENT_PROFILE = "studentProfile";
+   public static final String EDUCATIONAL_QUALIFICATIONS = "eduactionalQualifications";
+   public static final String MAIL_SENDER = "mailSender";
+   public static final String EVENTS = "events";
+   public static final String PLACED_STUDENT = "placedStudent";
+   public static final String FB_USER = "fbUser";
+   
+   // Services
+   public static final String APP_USER_SERVICE = "appUserService";
+   public static final String STUDENT_SERVICE = "studentService";
+   public static final String CONTACT_SERVICE = "contactsService";
+   public static final String STUDENT_PROFILE_SERVICE = "studentProfileService";
+   public static final String EDUCATIONAL_QUALIFICATIONS_SERVICE = "educationalQualificationService";
+   public static final String EVENT_SERVICE = "eventsService";
+   public static final String PLACED_STUDENT_SERVICE = "placedStudentService";
+   
+   //Factory Beans
+   public static final String SIMPLE_DATE_FORMAT = "simpleDateFormat";
+   public static final String DATE = "date";
+   public static final String SHORT = "short";
+   public static final String APP_MAIL_SERVICE = "appMailService";
+   public static final String VELOCITY_ENGINE = "velocityEngine";
+   public static final String JSON_OBJECT = "jsonObject";
 }
-
