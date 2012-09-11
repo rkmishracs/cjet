e1ef1690478cea77ef8fd62391683f8d63d94082
zarvis.a <zarvis.a@gmail.com>
2012-09-11 05:25:27 +0000

    Updated SocialController by Sudarsan

--- com.appcmc.site/src/java/com/appcmc/web/controller/SocialController.java
+++ com.appcmc.site/src/java/com/appcmc/web/controller/SocialController.java
@@ -7,6 +7,7 @@ package com.appcmc.web.controller;
 import com.appcmc.context.id.names.ContextIdNames;
 import com.appcmc.domain.sub.FBUser;
 import com.appcmc.utils.AppContext;
+import com.appcmc.web.forms.FaceBookStatusForm;
 import java.io.InputStream;
 import java.util.Properties;
 import org.apache.log4j.Logger;
@@ -20,8 +21,10 @@ import org.scribe.model.Verb;
 import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuthService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
 /**
@@ -36,10 +39,13 @@ public class SocialController {
    private Properties properties = null;
    private InputStream inputStream = null;
    private static final String PROTECTED_RESOURCE_URL = "https://graph.facebook.com/me";
+   private static final String SCOPE = "email,offline_access,user_status,publish_actions,publish_stream,read_friendlists";
    private static final Token EMPTY_TOKEN = null;
    private OAuthService service;
+   private Token accessToken = null;
    private FBUser fbUser = null;
-
+   private String fbUserId = null;
+   private Verifier verifier = null;
    @RequestMapping(method = RequestMethod.GET)
    public String showFaceBookHome(WebRequest request) {
       LOG.debug("==============In Social Controller=========");
@@ -58,7 +64,9 @@ public class SocialController {
                  .build();
 
          String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
-         return "redirect:" + authorizationUrl;
+         
+         LOG.debug("AuthorizationURL ===================="+authorizationUrl);
+         return "redirect:" + authorizationUrl+"&scope="+SCOPE;
 
       } catch (Exception exception) {
          LOG.debug("SocialController", exception);
@@ -77,10 +85,10 @@ public class SocialController {
    }
 
    @RequestMapping(method = RequestMethod.GET, params = {"code"})
-   public String fbLogin(WebRequest request, String code) {
-      Verifier verifier = new Verifier(request.getParameter("code"));
+   public String fbLogin(WebRequest request, String code, @ModelAttribute FaceBookStatusForm faceBookStatusForm) {
+      verifier = new Verifier(request.getParameter("code"));
 
-      Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+      accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
 
       OAuthRequest oAuthRequest = new OAuthRequest(Verb.GET,
               PROTECTED_RESOURCE_URL);
@@ -90,32 +98,64 @@ public class SocialController {
       JSONObject jSONObject = null;
       try {
          jSONObject = new JSONObject(oAuthResponse.getBody());
-         //LOG.debug(jSONObject.toString());
          
+         LOG.debug(jSONObject.toString());
+
          fbUser = (FBUser) AppContext.APPCONTEXT.getBean(ContextIdNames.FB_USER);
          fbUser.setId(Long.parseLong(jSONObject.getString("id")));
-         fbUser.setFirstName(jSONObject.getString("first_name"));
-         fbUser.setName(jSONObject.getString("name"));
-         fbUser.setTimeZone(jSONObject.getString("timezone"));
-         fbUser.setUserName(jSONObject.getString("username"));
+         //fbUser.setFirstName(jSONObject.getString("first_name"));
+         //fbUser.setName(jSONObject.getString("name"));
+         //fbUser.setTimeZone(jSONObject.getString("timezone"));
+         //fbUser.setUserName(jSONObject.getString("username"));
          //fbUser.setEmail(jSONObject.getString("email"));
-         fbUser.setVerified(Boolean.parseBoolean(jSONObject
-                 .getString("verified")));
-         fbUser.setLocale(jSONObject.getString("locale"));
-         fbUser.setLink(jSONObject.getString("link"));
-         fbUser.setLastName(jSONObject.getString("last_name"));
+         //fbUser.setVerified(Boolean.parseBoolean(jSONObject
+           //      .getString("verified")));
+         //fbUser.setLocale(jSONObject.getString("locale"));
+         //fbUser.setLink(jSONObject.getString("link"));
+         //fbUser.setLastName(jSONObject.getString("last_name"));
          fbUser.setGender(jSONObject.getString("gender"));
-         fbUser.setUpdatedTime(jSONObject.getString("updated_time"));
+         //fbUser.setUpdatedTime(jSONObject.getString("updated_time"));
 
-         LOG.debug("FbUser : ================="+fbUser);
+         this.fbUserId = String.valueOf(fbUser.getId());
          
+         LOG.debug("FbUser : =================" + fbUser);
+
          request.setAttribute("fbUser", fbUser, WebRequest.SCOPE_SESSION);
          return "/avtar/fbProfileView";
-         
+
       } catch (Exception exception) {
          LOG.debug("SocialController", exception);
       }
 
       return "";
    }
+   
+   @RequestMapping(method = RequestMethod.POST,value = "/updateStatus")
+   @ResponseBody
+   public String postMessageToFb(@ModelAttribute FaceBookStatusForm faceBookStatusForm){
+      
+      String PROTECTED_RESOURCE_URL_FEED = "https://graph.facebook.com/"+this.fbUserId+"/feed";
+      
+      
+      service = new ServiceBuilder()
+                  .provider(FacebookApi.class)
+                 .apiKey(properties
+                 .getProperty("app.config.oauth.facebook.apikey"))
+                 .apiSecret(
+                 properties.getProperty("app.config.oauth.facebook.apisecret"))
+                 .callback(properties.getProperty("app.config.oauth.facebook.callback"))
+                 .build();           
+      
+      OAuthRequest request = new OAuthRequest(Verb.POST, PROTECTED_RESOURCE_URL_FEED);      
+
+      request.addHeader("Content-Type", "text/html");
+      request.addBodyParameter("message", faceBookStatusForm.getStatusMessage());
+      
+      service.signRequest(accessToken, request);
+      
+      Response response = request.send();
+      return response.getBody();
+   }
+   
+   
 }
