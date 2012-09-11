7926c65288efe992244357b477ec47582e1e4ce8
zarvis.a <zarvis.a@gmail.com>
2012-09-11 05:26:08 +0000

    added FaceBookStatusForm to the App By Sudarsan

---
+++ com.appcmc.site/src/java/com/appcmc/web/forms/FaceBookStatusForm.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appcmc.web.forms;
+
+/**
+ *
+ * @author Sudarsan
+ */
+public class FaceBookStatusForm {
+   
+   private String statusMessage;
+
+   public String getStatusMessage() {
+      return statusMessage;
+   }
+
+   public void setStatusMessage(String statusMessage) {
+      this.statusMessage = statusMessage;
+   }
+}
